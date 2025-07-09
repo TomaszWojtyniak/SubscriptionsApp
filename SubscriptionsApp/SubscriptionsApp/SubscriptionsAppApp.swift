9e39377dcb666fb9a46c8714f50e2154f2f39671
Tomasz Wojtyniak <wojtyniaktomek@gmail.com>
2025-07-09 20:21:53 +0200

    Init project

---
+++ SubscriptionsApp/SubscriptionsApp/SubscriptionsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionsAppApp.swift
+//  SubscriptionsApp
+//
+//  Created by Tomasz Wojtyniak on 09/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SubscriptionsApp/SubscriptionsAppTests/SubscriptionsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionsAppTests.swift
+//  SubscriptionsAppTests
+//
+//  Created by Tomasz Wojtyniak on 09/07/2025.
+//
+
+import Testing
+@testable import SubscriptionsApp
+
+struct SubscriptionsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
